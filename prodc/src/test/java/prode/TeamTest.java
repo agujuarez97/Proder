fec4e3769e8d72fea9184b3e928aae6bdb29f5e7
Agustin Juarez <agujuarez97@hotmail.com.ar>
2018-04-26 12:06:41 -0300

    Teams

--- prodc/src/test/java/prode/TeamTest.java
+++ prodc/src/test/java/prode/TeamTest.java
@@ -9,6 +9,26 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class UserTest {
-	
+public class TeamTest {
+  @Before
+  public void before(){
+    Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "root", "root");
+    System.out.println("TeamTest setup");
+    Base.openTransaction();
+  }2
+
+  @After
+  public void after(){
+      System.out.println("TeamTest tearDown");
+      Base.rollbackTransaction();
+      Base.close();
+  }
+
+  @Test
+  public void validatePrecenseOfTestnames(){
+      Team team = new Team();
+      team.set("teamname", "");
+
+      assertEquals(team.isValid(), false);
+  }
 }
