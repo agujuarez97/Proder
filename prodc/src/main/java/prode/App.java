e67df33bb07416e456ef1455335a58d907d41f43
GPlisga <gaston_plisga@hotmail.com>
2018-11-02 14:47:53 -0300

    #161487621 - Migration the route /global to MVC

--- prodc/src/main/java/prode/App.java
+++ prodc/src/main/java/prode/App.java
@@ -151,24 +151,7 @@ public class App {
 
 /*----------------------------------------------------------------------------------------------*/
 
-	get("/global", (req, res) -> {
-		requestsRanking.mark();
-		Map rG = new HashMap();
-		List<Score> top = Score.findBySQL("select user_id, sum(points) as points from scores group by user_id order by points desc;");
-		List<Map> p = new ArrayList<Map>();
-		for(int i = 0; i < top.size(); i++){
-			List<User> u = User.where("id = ?", top.get(i).get("user_id"));
-			Map a = new HashMap();
-			a.put("username", u.get(0).get("username"));
-			a.put("points", top.get(i).get("points"));
-			p.add(a);
-		}
-
-		rG.put("ranking", p);
-
-		return new ModelAndView(rG, "./views/global.html");
-	},  new MustacheTemplateEngine()
-	);
+	get("/global", punctuationControllers::global, new MustacheTemplateEngine());
 
 /*---------------------------------------------------------------------------------------------*/
 
