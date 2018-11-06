d879ebd37c24bda99b52b47ce1d95a20cd0fe959
Agustin Juarez <agujuarez3@gmail.com>
2018-11-06 00:10:04 -0300

    #161741641-clearApp

--- prodc/src/main/java/prode/App.java
+++ prodc/src/main/java/prode/App.java
@@ -8,54 +8,14 @@
 package prode;
 
 import org.javalite.activejdbc.Base;
-import java.util.List;
-import java.util.ArrayList;
-
 import controllers.*;
-
-import spark.Request;
-import spark.Response;
-import models.User;
-import models.Prediction;
-import models.Score;
-
 import static spark.Spark.*;
 import static spark.Spark.staticFileLocation;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
-import com.codahale.metrics.*;
-import java.util.concurrent.TimeUnit;
-
 
 public class App {
 
-	/**
-	* Variable la cual es un registro que almacena todas las metricas(cada una con su respectivo nombre)
-	*/
-	static final MetricRegistry metrics = new MetricRegistry();
-		
 	public static void main( String[] args ){
-
-	/**
-	* Metrica la cual va a verificar cuantas veces se a solicitado ver el ranking, estos nos sirve para comprobar
-	* si es una pagina importante del sistema
-	*/
-	Meter requestsRanking = metrics.meter("requests-ranking");
-
-	/**
-	* Metrica la cual verifica cuantas personas se han registrado en el sistema
-	*/
-	Meter requestsSignIn = metrics.meter("requests-Sign-In");
-
-	/**
-	* Metrica la cual es un contenedor que va a contener la cantidad de personas que se encuentran actualmente jugando
-	* cada vez que una persona acceda a la pagina play se aumenta en 1 y cada vez que la persona salga se descrementa en 1
-	*/
-	Counter numPeoplePlaying = metrics.counter(metrics.name(App.class, "number-of-people-on-the-play-page"));
 
 	staticFiles.location("/public");
 
@@ -91,16 +51,6 @@ public class App {
 
 	get("/punctuation", punctuationControllers::punctuation, new MustacheTemplateEngine());	
 
-	startReport();
 	
 	}
-
-	static void startReport() {
-		ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
-		.convertRatesTo(TimeUnit.SECONDS)
-		.convertDurationsTo(TimeUnit.MILLISECONDS)
-		.build();
-		reporter.start(1, TimeUnit.SECONDS);
-	}
-
 }
