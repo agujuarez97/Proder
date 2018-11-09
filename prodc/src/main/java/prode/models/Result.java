03ac63a22ada5bdc66dba16f794083afa8cc77ec
Agustin Juarez <agujuarez3@gmail.com>
2018-11-09 12:45:11 -0300

    #161523078-migration of the model Result.java to MVC

--- prodc/src/main/java/prode/models/Result.java
+++ prodc/src/main/java/prode/models/Result.java
@@ -6,6 +6,7 @@
 
 package models;
 
+import java.util.*;
 import org.javalite.activejdbc.Model;
 
 public class Result extends Model{
@@ -31,5 +32,22 @@ public class Result extends Model{
 	 */
 	public Result(int res){
 		set("result", res);
+	}
+
+	/**
+	 * @return the result
+	 */
+	public Integer getResult(){
+		return this.getInteger("result");
+	}
+
+	/**
+	* @return all the information of the result
+	*/
+	public Map getCompleteResult(){
+		Map m = new HashMap();
+		m.put("id", this.getId());
+		m.put("resultado", this.getResult());
+		return m;
 	}
 }
