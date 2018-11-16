caebb57a13f78f091549455e819a34efa1d6abdb
Agustin Juarez <agujuarez3@gmail.com>
2018-11-16 14:26:21 -0300

    #161996583 - aggregation of everything necessary to create a new schedule in a fixture

--- prodc/src/main/java/prode/controllers/administratorControllers.java
+++ prodc/src/main/java/prode/controllers/administratorControllers.java
@@ -14,6 +14,11 @@ public class administratorControllers{
 		Map m = new HashMap();
 		return new ModelAndView(m, "./views/registergame.html");
 	}
+
+	public static ModelAndView addschedule(Request request, Response response){
+		Map m = new HashMap();
+		return new ModelAndView(m, "./views/registerschedule.html");
+	}
 	
 	public static ModelAndView registergame(Request request, Response response){
 		Map m = new HashMap();
@@ -32,6 +37,22 @@ public class administratorControllers{
 		}
 		
 		return new ModelAndView(m, "./views/administrator.html");
+	}
+
+	public static ModelAndView registerschedule(Request request, Response response){
+		Map m = new HashMap();
+		
+		List<Fixture> fixture = Fixture.findBySQL("select * from fixtures where name = ?;", request.queryParams("fixture"));
+
+		if(fixture.size() > 0){
+			Schedure schedule = new Schedure((int)fixture.get(0).get("id"));
+			schedule.saveIt();
+			return new ModelAndView(m, "./views/administrator.html");
+			
+		} else {
+			m.put("error", "<div class='alert alert-danger'><strong>Error!</strong> Fixture inexistente.</div>");
+			return new ModelAndView(m, "./views/registerschedule.html");
+		}
 	}
 	
 }
