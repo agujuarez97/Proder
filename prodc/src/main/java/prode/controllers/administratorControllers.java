4d203720ed049ad1e8fb7f3df07a73fee7b7acfa
GPlisga <gaston_plisga@hotmail.com>
2018-11-16 00:36:52 -0300

    [#161996562] Implementation methods loadGame() and registergame() in administratorControllers.java

--- prodc/src/main/java/prode/controllers/administratorControllers.java
+++ prodc/src/main/java/prode/controllers/administratorControllers.java
@@ -10,4 +10,28 @@ import java.util.*;
 
 public class administratorControllers{
 
+	public static ModelAndView loadgame(Request request, Response response){
+		Map m = new HashMap();
+		return new ModelAndView(m, "./views/registergame.html");
+	}
+	
+	public static ModelAndView registergame(Request request, Response response){
+		Map m = new HashMap();
+		
+		List<Team> local = Team.findBySQL("select * from teams where name = ?;", request.queryParams("local"));
+		List<Team> visitor = Team.findBySQL("select * from teams where name = ?;", request.queryParams("visitor"));
+		
+		if(local.size() > 0 && visitor.size() > 0){
+		
+			Map team_local = local.get(0).getCompleteTeam();
+			Map team_visitor = visitor.get(0).getCompleteTeam();
+			
+			Game game = new Game(request.queryParams("date"), request.queryParams("hour"), 0, 0, (int)team_local.get("id"), (int)team_visitor.get("id"), Integer.parseInt(request.queryParams("schedule").toString()), 0);
+			game.saveIt();
+			
+		}
+		
+		return new ModelAndView(m, "./views/administrator.html");
+	}
+	
 }
