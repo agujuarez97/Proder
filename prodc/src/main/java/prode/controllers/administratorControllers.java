ec854b1628068d4db6a6c2672fb5a79b184cfb33
GPlisga <gaston_plisga@hotmail.com>
2018-11-15 23:26:30 -0300

    [#162002894] Creation of the administratorControllers.java

---
+++ prodc/src/main/java/prode/controllers/administratorControllers.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import models.*;
+import spark.Request;
+import spark.Response;
+
+import spark.ModelAndView;
+
+import java.util.*;
+
+public class administratorControllers{
+
+}
